380c2ef51124c91de00c0d83e6346f7473516a60
unknown <mursalinkhan552@gmail.com>
2022-08-23 08:05:24 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+// var messiObject = {
+//     name : "Lionel Messi",
+//     imageUrl : "images/messi (2).jpg",
+//     score : "36 Goals",
+//     assist : "120 Assist"
+// }
+// var neymerObject = {
+//     name : "Neymar Jr",
+//     imageUrl : "images/neymer.webp",
+//     score : "36 Goals",
+//     assist : "120 Assist"
+// }
+// var mbappeObject = {
+//     name : "Kylian Mbappé",
+//     imageUrl : "images/mbappe.jpg",
+//     score : "36 Goals",
+//     assist : "120 Assist"
+// }
+// var ramosObject = {
+//     name : "Sergio Ramos",
+//     imageUrl : "images/remos.png",
+//     score : "36 Goals",
+//     assist : "120 Assist"
+// }
+// var vitorObject = {
+//     name : "Vítor Machado",
+//     imageUrl : "images/psg1.png",
+//     score : "36 Goals",
+//     assist : "120 Assist"
+// }
+// var renatoObject = {
+//     name : "Renato Sanches",
+//     imageUrl : "images/psg2.png",
+//     score : "36 Goals",
+//     assist : "120 Assist"
+// }
+
+
+// function selectPlayers(player){
+//     const mainSection = document.getElementById('main-section');
+//     const div = document.createElement("div");
+//     div.innerHTML = `
+//    <div class = "grid grid>
+//    <div class="card mt-5" style="width: 18rem;">
+//    <img src="${player.imageUrl}" class="card-img-top" alt="...">
+//    <div class="card-body">
+//      <h5 class="card-title text-center">${player.name}</h5>
+//      <p class = "text-center"><small class = "text-muted">${player.score}</small>  .  <small class = "text-muted">${player.assist}</small></p>
+//      <button type="button" class="btn btn-primary btn-lg text-center">SELECT</button>
+     
+//      <!-- <a href="#" class="btn btn-primary text-center ps-5 pe-5">SELECT</a> -->
+//    </div>
+//    </div>
+//    </div>
+
+//     `;
+//     mainSection.appendChild(div)
+
+// }
+
+// selectPlayers(messiObject);
+// selectPlayers(neymerObject);
+// selectPlayers(mbappeObject);
+// selectPlayers(ramosObject);
+// selectPlayers(vitorObject);
+// selectPlayers(renatoObject);
