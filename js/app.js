0311e9aacfeb7b364bf6463ae77dfc07112f8842
unknown <mursalinkhan552@gmail.com>
2022-08-23 08:33:05 -0700

    js calculation completed

--- js/app.js
+++ js/app.js
@@ -30,3 +30,47 @@ function createNewLi(text) {
     ol.appendChild(li)
 }
 
+
+// ================= total cost section ==============
+
+// ------------- comon function ---------------
+function inputValueById(targetedElementId) {
+    const inputFeildElement = document.getElementById(targetedElementId);
+    const inputFeildValue = inputFeildElement.value;
+    const inputValue = parseFloat(inputFeildValue);
+    return inputValue;
+}
+
+
+const playerExpensiveCalculate = document.getElementById('calculate-player-expenxive-js')
+playerExpensiveCalculate.addEventListener('click', function () {
+    const perPlayerExpensive = inputValueById('per-player-expensive-js')
+    if (perPlayerExpensive >= 0)
+    
+    {
+        const totalPlayer = parseInt((document.getElementById("ol").childNodes.length) - 1);
+
+        const calculate = totalPlayer * perPlayerExpensive;
+        const totalPlayerExpensive = document.getElementById('total-player-expensive-js');
+        totalPlayerExpensive.innerText = calculate;
+    }
+    else {
+        alert('please fill input fiels!!!')
+    }
+})
+
+
+const calculateTotal = document.getElementById('calculate-total-js')
+calculateTotal.addEventListener('click', function () {
+    const totalPlayerExepensess = document.getElementById('total-player-expensive-js').innerText;
+    const managerCost = inputValueById('manager-expensive-js')
+    const coachCost = inputValueById('coach-expensive-js')
+    const totalPlayerCost = parseFloat(totalPlayerExepensess)
+    if ((managerCost >= 0) && (coachCost >= 0)) {
+        const totalCost = totalPlayerCost + managerCost + coachCost;
+        document.getElementById('total-expensive-js').innerText = totalCost;
+    }
+    else {
+        alert('please fill input field')
+    }
+})
